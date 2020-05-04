99bcf00dc503ab7180a3dec8bdc2475f850e0633
sepkeuchenius <septimber@gmail.com>
2020-05-04 14:41:58 +0200

    tones

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var myGamePiece;
 var mousex;
 var mousey
 function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGamePiece = new component(30, 30, "red", (dimension[0] - 270 )/ 2, (dimension[1] - 270 )/ 2);
     myGameArea.start();
 }
 var dimension = [document.documentElement.clientWidth, document.documentElement.clientHeight];
@@ -18,7 +18,7 @@ var myGameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
         run()
-        this.interval2 = setInterval(updateDirection, 5000)
+        this.interval2 = setInterval(updateDirection, 1000)
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -40,25 +40,60 @@ function component(width, height, color, x, y) {
     }
     this.newPos = function() {
       if((this.x - mousex)**2 + (this.y - mousey)**2 < 200){console.log('hit')}
-      if(this.x < 0 || this.x > myGameArea.canvas.width){
-        this.speedX *= -1
-      }
-      if(this.y < 0 || this.y > myGameArea.canvas.height){
-        this.speedY *= -1
-      }
+      // if(this.x < 0 || this.x > myGameArea.canvas.width){
+      //   this.speedX *= -1
+      // }
+      // if(this.y < 0 || this.y > myGameArea.canvas.height){
+      //   this.speedY *= -1
+      // }
         this.x += this.speedX;
         this.y += this.speedY;
     }
 }
+var sec = 0;
+var n = [0,-1]
+var s = [0, 1]
+var w = [-1,0]
+var e = [1,0]
+var directionUpdates = {
+  1: s,
+  3: e,
+  5: n,
+  7: w,
+  9: s,
+  11: n,
+  12: s,
+  14: e,
+  17: s,
+  19: e,
+  20: n
+}
+var high_beeps = [2, 5, 9, 14, 20, 22, 25, 28, 29, 30, 34, 39, 41, 48, 55, 59]
+var low_beeps = [3, 8, 12, 16, 19, 24, 31, 35, 40, 45, 50, 52, 53, 57]
+var beeps = {}
+for(var i in low_beeps){
+  beeps[low_beeps[i]] = 1
+  if(high_beeps[i]){
+    beeps[high_beeps[i]] = 2
+  }
+}
+var audio_low = document.getElementById('audio-low')
+var audio_high = document.getElementById('audio-high')
 function updateDirection(){
-  // console.log(missed)
-  num = Math.random() * 2 + 2
-  num2 = Math.random() * 2 + 2
-  num *=  Math.floor(Math.random()*2) == 1 ? 1 : -1;
-  num2 *=  Math.floor(Math.random()*2) == 1 ? 1 : -1;
-  myGamePiece.speedX = num
-  myGamePiece.speedY = num2
-
+  if(directionUpdates[sec]){
+    myGamePiece.speedX = directionUpdates[sec][0] * 1.5
+    myGamePiece.speedY = directionUpdates[sec][1] * 1.5
+  }
+  if(beeps[sec]){
+    console.log(beeps[sec])
+    if(beeps[sec] == 1){
+      audio_low.play()
+    }
+    else if(beeps[sec] == 2){
+      audio_high.play()
+    }
+  }
+  sec++
 }
 function updateGameArea() {
     myGameArea.clear();
@@ -82,12 +117,12 @@ function moveright() {
     myGamePiece.speedX += 1;
 }
 function run(){
-  num = Math.random() * 2
-  num2 =Math.random() * 2
-  num *=  Math.floor(Math.random()*2) == 1 ? 1 : -1;
-  num2 *=  Math.floor(Math.random()*2) == 1 ? 1 : -1;
-  myGamePiece.speedX = num
-  myGamePiece.speedY = num2
+  // num = Math.random() * 2
+  // num2 =Math.random() * 2
+  // num *=  Math.floor(Math.random()*2) == 1 ? 1 : -1;
+  // num2 *=  Math.floor(Math.random()*2) == 1 ? 1 : -1;
+  // myGamePiece.speedX = num
+  // myGamePiece.speedY = num2
 }
 function handleMouseMove(event) {
       mousex =  event.clientX;
